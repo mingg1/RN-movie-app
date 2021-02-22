f0c71fa0bd07b24f72a5620f920dab7e77831725
mingg1 <comg2843@gmail.com>
2021-02-22 12:08:17 +0200

    added navigators

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+import { StyleSheet, Text, View, Image, StatusBar } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { Asset } from "expo-asset";
+import Stack from "./navigation/Stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+const cacheImages = (images) =>
+  images.map((image) => {
+    // if an iamge is url
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      // if an image is module
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
+
+const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
+const App = () => {
+  const [isReady, setIsReady] = useState(false);
+
+  const loadAssets = () => {
+    const images = cacheImages([
+      "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1027&q=80",
+      require("./assets/splash.png"), // <= 이게 모듈,
+    ]);
+    const fonts = cacheFonts([Ionicons.font]);
+    return Promise.all([...images, ...fonts]);
+  };
+
+  const onFinish = () => {
+    setIsReady(true);
+    console.log(isReady);
+  };
+  return isReady ? (
+    <>
+      <NavigationContainer>
+        <Stack />
+      </NavigationContainer>
+      <StatusBar barStyle="light-content" />
+    </>
+  ) : (
+    <AppLoading startAsync={loadAssets} onFinish={onFinish} onError={console.error} />
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default App;
